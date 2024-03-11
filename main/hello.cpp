5778c2e3232f8cf4bd9ca3c7b160a111a889a980
SarangKumar <sarangkumar1578@gmail.com>
2024-03-11 12:58:21 +0530

    updated cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and run Jenkins lab\n";
   cout << "Sarang Kumar: PES1UG21CS537\n";
   return 0;
 }
